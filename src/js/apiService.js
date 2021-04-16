e7eee4a6102adc751c8fe0524d2a7c8e18a488a3
Andrey Cherniak <baza904@gmail.com>
2021-04-16 17:05:38 +0300

    add key and url const

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,19 +1,13 @@
-// export default function fetchImage(searchQuery) {
-//   const url = `${BASE_URL}/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${pageNumber}&per_page=12&key=${API_KEY}`;
-//   return fetch(url).then(response => response.json());
-// }
+const API_KEY = '21175244-7c66133e2371767f9955ec31b';
+const BASE_URL = 'https://pixabay.com/api/';
 
 class FetchImageApi {
-  #key = '21175244-7c66133e2371767f9955ec31b';
-
   constructor() {
     this.searchQuery = '';
     this.pageNumber = 1;
   }
   fetchImage() {
-    const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${
-      this.searchQuery
-    }&page=${this.pageNumber}&per_page=12&key=${this.#key}`;
+    const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.pageNumber}&per_page=12&key=${API_KEY}`;
     return fetch(url)
       .then(r => r.json())
       .then(data => {
